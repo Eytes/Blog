be7d27c648f46cbcb2433db0a3a795f3282746fc
Eytes <yura.shamanov2000@bk.ru>
2024-01-01 12:57:28 +0200

    создание эндпоинта получения автора

--- post_manager/post_manager/app/routers/author.py
+++ post_manager/post_manager/app/routers/author.py
@@ -1,8 +1,10 @@
+from uuid import UUID, uuid4
 from fastapi import APIRouter
 
 from post_manager.app.schemas.author import (
     CreateAuthor,
     UpdateAuthor,
+    Author,
 )
 
 router = APIRouter(
@@ -16,6 +18,9 @@ def create(new_author: CreateAuthor):
     """
     Эндпоинт для создания автора
     """
+    # TODO: отправить запрос в БД на создание
+    # TODO: обработать ответ от БД
+    # TODO: выдать результат
     pass
 
 
@@ -24,4 +29,18 @@ def update(new_author_data: UpdateAuthor):
     """
     Эндпоинт обновления данных автора
     """
+    # TODO: отправить запрос в БД на обновление
+    # TODO: обработать ответ от БД
+    # TODO: выдать результат
+    pass
+
+
+@router.get("/{author_id}")
+def get(author_id: UUID) -> Author:
+    """
+    Получение автора по id
+    """
+    # TODO: отправить запрос в БД для получение автора
+    # TODO: обработать ответ от БД
+    # TODO: выдать результат
     pass
