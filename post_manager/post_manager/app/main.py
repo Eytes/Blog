91fa397ba756a4e5e7470fca6614c826cef9fa0e
Eytes <yura.shamanov2000@bk.ru>
2024-01-01 12:59:07 +0200

    добавление точки запуска приложения

--- post_manager/post_manager/app/main.py
+++ post_manager/post_manager/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 from post_manager.app.routers import (
     author,
@@ -6,3 +7,6 @@ from post_manager.app.routers import (
 
 app = FastAPI()
 app.include_router(author.router)
+
+if __name__ == '__main__':
+    uvicorn.run("post_manager.app.main:app", reload=True)
