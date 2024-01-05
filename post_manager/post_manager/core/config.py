bb165723071d4040084ea9f009294b53d19580d9
Eytes <yura.shamanov2000@bk.ru>
2024-01-06 00:43:22 +0200

    создание docker-compose.yml для запуска сервиса post_manager и бд

--- post_manager/post_manager/core/config.py
+++ post_manager/post_manager/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 
 class DataBaseSettings(BaseModel):
-    url: str = "postgresql+asyncpg://test:test@localhost:5431/test"
+    url: str = "postgresql+asyncpg://eytes:qwerty@postgres/blog"
     echo: bool = True  # TODO: Заменить на False при публикации на сервере
 
 
