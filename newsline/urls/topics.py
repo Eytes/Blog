13e5e39304053f8db06b71f54f96d07608d03d70
Eytes <yura.shamanov2000@bk.ru>
2024-01-16 16:54:37 +0200

    добавление возможности создавать и просматривать тематики

--- newsline/urls/topics.py
+++ newsline/urls/topics.py
@@ -2,9 +2,22 @@ import requests
 import streamlit as st
 
 from config import topic_prefix
+from schemas.topics import TopicCreate
 
 
 @st.cache_data(ttl=60)
-def get_all_topics():
+def get_all_topic_names() -> list[str]:
     topics_raw = requests.get(topic_prefix + "/")
     return [topic.get("name") for topic in topics_raw.json()]
+
+
+def create_topic(new_topic: TopicCreate):
+    response = requests.post(
+        topic_prefix + "/",
+        json=new_topic.model_dump(),
+    )
+    match response.status_code:
+        case 201:
+            return "Success"
+        case _:
+            return response.json().get("detail")
