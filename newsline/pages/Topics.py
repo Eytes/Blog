6c36b135d68b28042339789683d6d47119e1b9ea
Eytes <yura.shamanov2000@bk.ru>
2024-01-16 17:03:38 +0200

    выделение логики получение тематик в отдельную функцию

--- newsline/pages/Topics.py
+++ newsline/pages/Topics.py
@@ -2,14 +2,14 @@ import pandas as pd
 import streamlit as st
 
 from forms.topics.create import TopicCreateForm
-from urls.topics import get_all_topic_names
+from urls.topics import get_topics_name
 
 st.title("Topics")
 
 topics_tab, create_tab = st.tabs(["Topics", "Create"])
 
 with topics_tab:
-    topic_names = get_all_topic_names()
+    topic_names = get_topics_name()
     df = pd.DataFrame(
         {
             "name": topic_names,
--- newsline/urls/topics.py
+++ newsline/urls/topics.py
@@ -2,16 +2,24 @@ import requests
 import streamlit as st
 
 from config import topic_prefix
-from schemas.topics import TopicCreate
+from schemas.topics import TopicCreate, Topic
 
 
 @st.cache_data(ttl=60)
-def get_all_topic_names() -> list[str]:
+def get_topics() -> list[Topic]:
+    """Получить все тематики"""
     topics_raw = requests.get(topic_prefix + "/")
-    return [topic.get("name") for topic in topics_raw.json()]
+    return list(map(lambda topic: Topic(**topic), topics_raw.json()))
+
+
+def get_topics_name() -> list[str]:
+    """Получить все названия тематик"""
+    topics = get_topics()
+    return [topic.name for topic in topics]
 
 
 def create_topic(new_topic: TopicCreate):
+    """Создание тематики"""
     response = requests.post(
         topic_prefix + "/",
         json=new_topic.model_dump(),
